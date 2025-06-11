eb97550fabf69531a97d12157f16595c6f5414e2
smanaman <sanvatsman@gmail.com>
2025-06-11 17:22:21 +0530

    mogodb-crud

--- app.js
+++ app.js
@@ -1,50 +1,127 @@
-const express=require('express');
-const app=express();
-const path=require('path')
-const db =require('./config/db')
-const admin=require('./models/admintbl')
+const express = require('express');
+const app = express();
+const path = require('path')
+const db = require('./config/db')
+const admin = require('./models/admintbl')
 
-app.set('view engine','ejs')
-app.use('views',express.static(path.join(__dirname,'views')))
+app.set('view engine', 'ejs')
+app.use('views', express.static(path.join(__dirname, 'views')))
 app.use(express.urlencoded())
 
 
-app.get('/',(req,res)=>{
+app.get('/adddata', (req, res) => {
 
     res.render('form')
 
 
-}) 
-
-app.get('/showdata',async(req,res)=>{
-    try{
-
-    const users=await admin.find()
-        res.render('showdata',{users})
-    }
-    catch(error){
-        console.log(error);
-        
-    }
 })
 
-app.post('/adddata',(req,res)=>{
 
-    const {name,email,phone,password,city,gender,hobby}=req.body;
 
-admin.create({
-    name:name,
-    email:email,
-    phone:phone,
-    password:password,
-    city:city,
-    gender:gender,
-    hobby:hobby
-})
+app.post('/adddata', (req, res) => {
+
+    const { name,
+        email,
+        phone,
+        address,
+        city,
+        zip,
+        country,
+        shipping,
+        cardname,
+        cardnumber,
+        expiry,
+        cvv } = req.body;
+
+    admin.create({
+       name: name,
+        email: email,
+        phone: phone,
+        address: address,
+        city: city,
+        zip: zip,
+        country: country,
+        shipping: shipping,
+        cardname: cardname,
+        cardnumber: cardnumber,
+        expiry: expiry,
+        cvv: cvv
+    })
     res.redirect('/')
 })
-app.listen(3000,()=>{
-    console.log('http://localhost:3000');
- 
+app.get('/', async (req, res) => {
+    try { 
+
+        const users = await admin.find()
+        res.render('showdata', { users })
+    }
+    catch (error) {
+        console.log(error);
+
+    }
+})
+app.get('/DeletData', async (req, res) => {
+    const id = req.query.id
+
+    await admin.findByIdAndDelete(id)
+
+
+    res.redirect('/')
+
+})
+
+app.get('/editdata', async (req, res) => {
+
+    const id = req.query.id
+    console.log('id', id);
+
+    const userupdate = await admin.findById(id)
+
+    if (!userupdate) {
+        return res.status(404).redirect('404')
+    }
+    res.render('EditData', { userupdate })
+
+
+})
+
+app.post('/editdata', async (req, res) => {
+    const id = req.query.id
     
+    const {      name,
+        email,
+        phone, 
+        address,
+        city,
+        zip,
+        country,
+        shipping,
+        cardname,
+        cardnumber,
+        expiry,
+        cvv } = req.body
+
+    await admin.findByIdAndUpdate(id, {
+        name: name,
+        email: email,
+        phone: phone,
+        address: address,
+        city: city,
+        zip: zip,
+        country: country,
+        shipping: shipping,
+        cardname: cardname,
+        cardnumber: cardnumber,
+        expiry: expiry,
+        cvv: cvv
+
+    })
+    res.redirect('/')
 }) 
+
+
+    app.listen(3000, () => {
+        console.log('http://localhost:3000');
+
+
+    }) 
